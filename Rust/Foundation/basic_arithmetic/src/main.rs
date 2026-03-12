73005d6f27750bc606bc79d285b4009215b7e11b
Lucio <48389674+NeonNingen@users.noreply.github.com>
2026-03-12 12:25:17 +0000

    Completed Basic Arithmetic

--- Rust/Foundation/basic_arithmetic/src/main.rs
+++ Rust/Foundation/basic_arithmetic/src/main.rs
@@ -1,10 +1,23 @@
-// use prompted::input;
+// use prompted::input; Crate with python like input
+use std::io;
 
 fn main() {
     println!("Please enter 2 number, to be added together");
 
-    println!("Num 1");
+    let mut num: u8 = 1;
+    let num1 = take_int(num);
+    num = 2;
+    let num2 = take_int(num);
 
-    let mut num1 = String::new();
+    println!("{}", num1 + num2);
+}
 
+// Function for int64 input
+fn take_int(num: u8) -> i64 {
+    println!("Please input number {num}: ");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    return input.trim().parse().unwrap();
 }
